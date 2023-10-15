4de7dee2745a20b358c9a7ae5fadde03ed0e130f
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-10-14 23:23:18 -0300

    finalizado
    conclui o treinamento sobre consumo e tratamento de dados de uma *API

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 async function buscaEndereco(cep) {
+    var mesagemErro = document.getElementById('erro');
+    mesagemErro.innerHTML = "";
     try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
-    console.log (erro  )
+        mesagemErro.innerHTML =`<p> CEP inválido. Tente novamente!</p>`
+    console.log (erro)
     }
 } 
 
-let ceps = ['01001000', '01001001' ]
-let conjutoCeps = ceps.map(valores => buscaEndereco(valores));
-
-Promise.all(conjutoCeps).then(respostas => console.log(respostas))
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco (cep.value));       // clica fora
 
